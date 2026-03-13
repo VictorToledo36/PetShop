147ae11d0b5be45557b144c78cdb1e5c705d7a7f
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-12 22:52:03 -0300

    ft: Configurando a tabela de agendamentos

---
+++ PetShop.Infrastructure/Persistence/Configurations/AgendamentoConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Infrastructure.Persistence.Configurations
+{
+    public class AgendamentoConfiguration : IEntityTypeConfiguration<Agendamento>
+    {
+        public void Configure(EntityTypeBuilder<Agendamento> builder)
+        {
+            builder.ToTable("Agendamentos");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.ApplicationUserId).HasMaxLength(450); 
+
+            builder.Property(a => a.DataSolicitada).IsRequired();
+
+            builder.Property(a => a.HorarioSolicitado).IsRequired().HasColumnType("time");
+
+            builder.Property(a => a.Status).IsRequired().HasConversion<string>();
+
+            builder.Property(a => a.ObservacaoCliente).HasMaxLength(500);
+
+            builder.Property(a => a.MotivoRecusa).HasMaxLength(500);
+
+            builder.Property(a => a.DataResposta).IsRequired(false);
+
+            builder.HasOne(a => a.Pet).WithMany().HasForeignKey(a => a.PetId).OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(a => a.Servico).WithMany().HasForeignKey(a => a.ServicoId).OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne<ApplicationUser>().WithMany().HasForeignKey(a => a.ApplicationUserId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
