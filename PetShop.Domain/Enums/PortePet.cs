6a65423caad9de9df00f6b7645ddfb9fa52278f7
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-14 04:09:49 -0300

    feat: Modificando arquivos enums

--- PetShop.Domain/Enums/PortePet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetShop.Domain.Enums;
-
-public enum PortePet
-{
-    Pequeno = 1,    // até 10kg
-    Medio = 2,      // 10kg a 20kg
-    Grande = 3,     // 20kg a 40kg
-    GrandePorte = 4 // acima de 40kg
-}
